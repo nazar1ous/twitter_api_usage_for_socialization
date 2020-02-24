59a3d3d44cbb09263123bfad863295b1d7de941d
Kuspys Turor <nazar0620@gmail.com>
2020-02-24 15:01:18 +0200

    added deletion of the web_map file

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from analysis_twitter_api import create_web_map_from_user_friends_location
-
+import os
 
 app = Flask(__name__)
 
@@ -8,19 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """
-    If user goes to main route, opens index.html
+    If user goes to the main route, opens index.html
     :return:
     """
     return render_template("index.html")
-    # return '<h1>Hello</h1>'
 
 
 @app.route("/web_map", methods=["POST"])
 def web_map():
     """
-    Create web map
+    Creates the web map of twitter friends
     :return: object of rendered template
     """
+    if os.path.exists('templates/web_map.html'):
+        os.remove('templates/web_map.html')
     screen_name = request.form.get("screen_name")
     create_web_map_from_user_friends_location(screen_name,
                                               'templates/web_map.html')
